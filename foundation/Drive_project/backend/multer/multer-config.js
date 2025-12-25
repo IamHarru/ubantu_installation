a4cf66334cf5bb13d206898fe7fff270097c7971
IamHaru <mayanksharma20004@gmail.com>
2025-12-25 22:26:09 +0530

    updating the multer to multers3

--- foundation/Drive_project/backend/multer/multer-config.js
+++ foundation/Drive_project/backend/multer/multer-config.js
@@ -1,11 +1,26 @@
 const multer = require('multer')
 const crypto = require('crypto')
 const path = require('path')
-const storage = multer.diskStorage({
-    destination: "./upload/",
-    filename: (req, file, cb) => {
-        cb(null, crypto.randomBytes(12).toString('hex') + path.extname(file.originalname));
-    }
+
+const { S3Client } = require("@aws-sdk/client-s3");
+const multerS3 = require("multer-s3");
+
+const s3 = new S3Client({
+  region: "us-east-1", // change if needed
 });
-const upload = multer({ storage });
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET, // <-- ONLY ONE BUCKET
+    key: (req, file, cb) => {
+      const fileName =
+        crypto.randomBytes(12).toString("hex") +
+        path.extname(file.originalname);
+      cb(null, `uploads/${fileName}`);
+    },
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+  }),
+});
+
 module.exports = upload;
